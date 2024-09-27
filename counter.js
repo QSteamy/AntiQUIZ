b1a215c3b17af69bfdabbb097dad9f2d28fa3b4e
Q.Steamy <42454595+QSteamy@users.noreply.github.com>
2024-09-27 17:51:21 +1000

    Update counter.js

--- counter.js
+++ counter.js
@@ -13,7 +13,8 @@ function onClickWin() {
     winBtn.style.backgroundColor = '#996c00';
     setTimeout(() => {
     	winBtn.style.backgroundColor = '#FFB401';  
-	}, 100)
+	}, 100);
+ innercount -= innercount;
 };
 
 
@@ -23,7 +24,7 @@ function onCLickLose() {
 	document.getElementById("loseclicks").innerHTML = loseclicks;
 	if(innercount == 3) {
 		active = active + 1 < items.length ?  active + 1 : active;
-		innercount -= 3;
+		innercount -= innercount;
 	};
     loadShow();
     failBtn.style.backgroundColor = '#991d00';
